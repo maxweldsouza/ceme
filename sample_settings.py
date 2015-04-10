d25e977c4b6677b864379c2a9a11e0adad7eae27
Maxwel Dsouza <maxellusionist@gmail.com>
2015-04-10 17:12:37 +0530

    added sample_settings.py

---
+++ sample_settings.py
@@ -0,0 +1,9 @@
+xsrf_cookie = 'somenameforthecookie'
+cookie_secret = 'somerandomstringforsecurity'
+ssl = False
+port = 8443
+
+db_host = "localhost"
+db_name = "databasename"
+db_user = "username"
+db_password = "password"
